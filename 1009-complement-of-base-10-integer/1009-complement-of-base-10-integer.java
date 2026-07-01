b1011a1d329ea681d20b66baaf31028a4c10efab
Adhithya Pranave <151059501+Adhithyapranave07@users.noreply.github.com>
2026-07-01 11:29:49 +0530

    Time: 1 ms (20.22%), Space: 42.2 MB (33.41%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int bitwiseComplement(int num) {
+        if( num == 0 ) return 1;
+          StringBuilder sb = new StringBuilder();
+        while( num > 0 ){
+            if((num&1) == 1 ) sb.append("0");
+            else sb.append("1");
+
+            num = num >> 1 ;
+    
+        }
+
+        return Integer.parseInt(sb.reverse().toString(),2);
+
+    }
+}
