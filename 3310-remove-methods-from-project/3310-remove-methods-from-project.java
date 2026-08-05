714ed40a6ddcbd9149a2f00823503be27b17042f
Adhithya Pranave <151059501+Adhithyapranave07@users.noreply.github.com>
2026-08-05 09:53:46 +0530

    Time: 93 ms (36.54%), Space: 287.9 MB (50%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for( int i = 0 ; i < n ; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for( int[] a : invocations){
+            graph.get(a[0]).add(a[1]);
+        }
+  
+        boolean[] visited = new boolean[n];
+        List<Integer> list = new ArrayList<>();
+ 
+        visited[k] = true ;
+        dfs(k,visited,graph);
+
+        // System.out.println(set);
+        boolean cr = true ;
+        for( int i = 0 ; i < n ; i++){
+           if(!visited[i]) list.add(i);
+            for( int nei : graph.get(i)){
+                if(!visited[i] && visited[nei]){
+                    cr = false ;
+                    break;
+                }
+            }
+        }
+        
+        if( !cr) {
+            List<Integer> list1 = new ArrayList<>();
+            for( int i = 0 ; i < n ; i++) list1.add(i);
+            return list1 ;
+        }
+        return list ;
+        
+    }
+
+    public void dfs( int i , boolean[] visited ,  List<List<Integer>> graph ){
+   
+
+        for( int node : graph.get(i)){
+            if(!visited[node]){
+                visited[node] =  true ;
+                dfs(node,visited,graph);
+            }
+        }    
+    }
+}
